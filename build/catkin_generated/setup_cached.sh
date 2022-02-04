47d857583b90a4928fc2be6795c67c2dba3329d9
firat helvaci <firat_helvaci@hotmail.com>
2022-02-05 01:48:35 +0300

    Graph added but showing values left

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -9,6 +9,7 @@
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/frat/catkin_ws/src/SEA_GUI/build/devel:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH="/home/frat/catkin_ws/src/SEA_GUI/build/devel/lib:$LD_LIBRARY_PATH"
+export PATH='/opt/ros/noetic/bin:/home/frat/.local/bin:/home/frat/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
 export PKG_CONFIG_PATH="/home/frat/catkin_ws/src/SEA_GUI/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/frat/catkin_ws/src/SEA_GUI/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/frat/catkin_ws/src/SEA_GUI:$ROS_PACKAGE_PATH"
--- src/GraphGUI.py
+++ src/GraphGUI.py
@@ -4,12 +4,14 @@ from PyQt5.uic import loadUi
 from matplotlib.backends.backend_qt5agg import (NavigationToolbar2QT as NavigationToolbar)
 import matplotlib.pyplot as plt
 import numpy as np
+import random
+
 
 class ui_GraphGUI(QMainWindow):
     def __init__(self):
         super(ui_GraphGUI, self).__init__()
         loadUi("/home/frat/catkin_ws/src/SEA_GUI/Qt5/GraphWindow.ui",self)
-        
+
         
         #self.addToolBar(NavigationToolbar(self.MplWidget.canvas, self))
 
@@ -18,27 +20,27 @@ class ui_GraphGUI(QMainWindow):
         f = df1["Hız"]
         t= df1["Time"]
 
-        self.deneme.canvas.axes1.clear()
-        self.deneme.canvas.axes1.plot(t,f)
+        self.MplWidget.canvas.axes1.clear()
+        self.MplWidget.canvas.axes1.plot(t,f)
 
         f = df2["Hız"]
         t= df2["Time"]
 
-        self.deneme.canvas.axes2.clear()
-        self.deneme.canvas.axes2.plot(t,f )
+        self.MplWidget.canvas.axes2.clear()
+        self.MplWidget.canvas.axes2.plot(t,f )
 
         f = df3["Hız"]
         t= df3["Time"]
 
-        self.deneme.canvas.axes3.clear()
-        self.deneme.canvas.axes3.plot(t,f )
+        self.MplWidget.canvas.axes3.clear()
+        self.MplWidget.canvas.axes3.plot(t,f )
 
         f = df4["Hız"]
         t= df4["Time"]
 
-        self.deneme.canvas.axes4.clear()
-        self.deneme.canvas.axes4.plot(t,f )
+        self.MplWidget.canvas.axes4.clear()
+        self.MplWidget.canvas.axes4.plot(t,f )
 
 
-        self.deneme.canvas.draw()
+        self.MplWidget.canvas.draw()
 
